3c9f6f93f7d93952698d4683214c2f6dd8505c79
neriosoft <alabaneri@gmail.com>
2023-08-31 06:49:40 +0100

    Added dynamic url, redirect and reverse function

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
-def monthly_challenge(request, month):
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!!!",
+    "february":"Walk for at least 20 minutes every day!!!",
+    "march": "Learn Django for at least 30 minutes daily!!!",
+    "april": "Evaluate yourself before you sleep daily!!!",
+    "may": "Get yourself some fruits to eat in the morning",
+    "june": "Try send some job proposal thrice a week to potential clients",
+    "july": "Do follow up calls to your students on their assignments",
+    "august": "Get yourself some new trousers to look takeaway!!!",
+    "september": "Ensure your electronics are turned off before going to bed daily!!!",
+    "october": "Always check your to-do list before you go out daily!!!",
+    "november": "Make new friends to engage at your leisure time daily!!!",
+    "december": "Finally you can rest and merry!!!"
+}
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
     
-    challenge_text = None
+    if month > len(months):
+        return HttpResponseNotFound("The number is out of range for the intended month!!!")
     
-    if month == "january":
-        challenge_text = "Eat no meat for the entire month!!!"
-        
-    elif month == "february":
-        challenge_text = "Walk for at least 20 minutes every day!!!"
-        
-    elif month == "march":
-        challenge_text = "Learn Django for at least 30 minutes daily!!!"
-        
-    else:
-        return HttpResponseNotFound("This month is not supported!!!")
-        
-    return HttpResponse(challenge_text)
+    redirect_month = months[month-1]
+    redirect_url = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_url)
+
+    
+def monthly_challenge(request, month):  
+    
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month not supported!!!")
+    
+   
 
  
